026f2b465a2e6d58113514c8c4f2ff9831c9b17d
kaisa <kaisa.koskinen3@metropolia.fi>
2026-02-04 11:43:13 +0200

    tehtava1

--- viikko1/tehtava1.2.py
+++ viikko1/tehtava1.2.py
@@ -1,3 +1,12 @@
 nimi = input("Anna oma nimesi: ")
 
-print("Hello", nimi)
+print("Hello", nimi+"!")
+
+print("Hello " + nimi + "!")
+
+print(f"Hello, {nimi}!")
+
+nimi2 = "Kaisa"
+
+print(f"Hello " + nimi + " ja " + nimi2 + "!")
+
